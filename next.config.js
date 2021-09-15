003d181f27ee04028b6a9da87700b8b0499d00a8
Luís Fernando <luis.rodrigues@ewave.com.br>
2021-09-15 18:31:00 -0300

    removing environment variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     env: {
-        DIALECT: process.env.DIALECT,
         HOST: process.env.DB_HOST,
         USERNAME: process.env.DB_USERNAME,
         PASSWORD: process.env.DB_PASSWORD,
